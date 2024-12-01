4d1912b1be5979ff60e988822ff0eca902ba436f
olegmakeienko <molva83@gmail.com>
2024-12-01 13:55:10 +0100

    Initial commit

---
+++ src/main/java/com/makeienko/laddstation/LaddstationApplication.java
@@ -0,0 +1,13 @@
+package com.makeienko.laddstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaddstationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaddstationApplication.class, args);
+    }
+
+}
