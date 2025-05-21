218d0a74dfa196796235cb7d4d6177e2135220eb
olegmakeienko <molva83@gmail.com>
2025-05-21 14:28:18 +0200

    created simple test void testChargeBatteryDirect() there request sends atLeastOnce (will be uppgrade in future)

--- src/test/java/com/makeienko/laddstation/service/ChargingServiceImplTest.java
+++ src/test/java/com/makeienko/laddstation/service/ChargingServiceImplTest.java
@@ -2,6 +2,8 @@ package com.makeienko.laddstation.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -23,29 +25,31 @@ public class ChargingServiceImplTest {
 
     @InjectMocks
     private ChargingServiceImpl chargingService;
-
+    private InfoResponse expectedResponse;
     private ObjectMapper objectMapper = new ObjectMapper();
+    private String expectedJsonResponse;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws JsonProcessingException {
         MockitoAnnotations.openMocks(this);
-    }
 
-    @Test
-    void testFetchAndDeserializeInfo() throws Exception {
         //skapa en förväntat InfoResponse objekt
-        InfoResponse expectedResponse = new InfoResponse();
-        expectedResponse.setBaseCurrentLoad(10.5);
+        expectedResponse = new InfoResponse();
+        expectedResponse.setBaseCurrentLoad(2.0);
         expectedResponse.setBatteryCapacityKWh(20.0);
         expectedResponse.setSimTimeHour(14);
         expectedResponse.setSimTimeMin(30);
         expectedResponse.setEvBatteryChargeStartStopp(true);
 
-        //konvertera till JSON-sträng
-        String mockJsonResponse = objectMapper.writeValueAsString(expectedResponse);
+        //konvertera till JSON-sträng för återanvändnig
+        expectedJsonResponse = objectMapper.writeValueAsString(expectedResponse);
+    }
+
+    @Test
+    void testFetchAndDeserializeInfo() {
 
         //konfigera mock
-        when(restTemplate.getForObject("http://127.0.0.1:5001/info", String.class)).thenReturn(mockJsonResponse);
+        when(restTemplate.getForObject("http://127.0.0.1:5001/info", String.class)).thenReturn(expectedJsonResponse);
 
         //exekvera metoden
         InfoResponse actualResponse = chargingService.fetchAndDeserializeInfo();
@@ -67,7 +71,12 @@ public class ChargingServiceImplTest {
 
         
         //konfiguera RestTemplate mock attt returera den simulerade Infooresponse
-        //when(restTemplate.getForObject("http://127.0.0.1:5001/info", String.class)).thenReturn("{\"simTimeHour\": 10, \"simTimeMin\": 0, \"baseCurrentLoad\": 5.0, \"batteryCapacityKWh\": 20.0, \"evBatteryChargeStartStopp\": false}");
+        when(restTemplate.getForObject("http://127.0.0.1:5001/info", String.class)).thenReturn(expectedJsonResponse);
+        // Exekvera metoden
+        chargingService.chargeBatteryDirect();
+
+        //verifiera att metoden anropar RestTemplate
+        verify(restTemplate, atLeastOnce()).getForObject("http://127.0.0.1:5001/info", String.class);
         
     }
 
