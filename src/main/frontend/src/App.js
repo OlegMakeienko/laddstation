f1b77e2a5273898a35f8a1e84d53f9c373597c91
olegmakeienko <molva83@gmail.com>
2025-05-29 08:23:41 +0200

    clean frontend is ready to make a new app

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World from React!</h1>
+      <p>Laddstation Frontend</p>
+    </div>
+  );
+}
+
+export default App; 
